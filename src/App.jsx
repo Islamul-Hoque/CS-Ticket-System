38ee9b3bee936bc2c98d0aa3f9b9492d3b03efc9
Islamul-Hoque <islamulhoque2006@gmail.com>
2026-03-04 01:48:49 +0600

    Integrate Banner section to display In-Progress and Resolved ticket counts with gradient backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { ToastContainer} from 'react-toastify';
+import { Suspense, useState } from 'react';
+
 import Navbar from "./Components/Navbar/Navbar";
+import Banner from './Components/Banner/Banner'
 
 function App() {
-
+    const [ inProgressCount, setInProgressCount ] = useState(0) 
+  const [ selectedTicket, setSelectedTicket ] = useState([]) 
+  const [ resolvedCount, setResolvedCount ] = useState(0) 
+  const [ resolvedTicket, setResolvedTicket ] = useState([]) 
+  const [ allTicket, setAllTicket ] = useState([]) 
   return (
     <>
       <Navbar></Navbar>
-      
+      <Banner 
+        resolvedCount={resolvedCount}
+        inProgressCount={inProgressCount} >
+      </Banner>
 
       
     </>
---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import bannerIMG1 from '../../assets/vector1.png'
+import bannerIMG2 from '../../assets/vector2.png'
+const Banner = () => {
+
+    return (
+        <div className='bg-[#f7f7f7]'>
+            <div className='max-w-7xl mx-auto'>
+                <div className='px-[2rem] py-[2rem] text-white h-100 md:h-65 flex flex-col md:flex-row gap-4 justify-center'>
+                    <div className='relative bg-[linear-gradient(125.07deg,#632ee3,#9f62f2_100%)] w-full h-full flex flex-col justify-center items-center rounded-[0.6rem]'>
+                        <img className='absolute left-0 bottom-0 w-[40%] md:w-auto h-40 md:h-[100%]' src={bannerIMG1} alt="banner image-1" />
+                        <h4 className='text-[1.2rem]'>In-Progress</h4>
+                        
+                        <img className='absolute right-0 bottom-0 w-[40%] md:w-auto h-40 md:h-[100%]' src={bannerIMG2} alt="banner image-2" />
+                    </div>
+
+                    <div className='relative bg-[linear-gradient(125.07deg,#54cf68,#00827a_100%)] w-full h-full flex flex-col justify-center items-center rounded-[0.6rem]'>
+                        <img className='absolute left-0 bottom-0 w-[40%] md:w-auto h-40 md:h-[100%]' src={bannerIMG1} alt="banner image-1" />
+                        <h4 className='text-[1.2rem]'>Resolved</h4>
+                     
+                        <img className='absolute right-0 bottom-0  w-[40%] md:w-auto h-40 md:h-[100%]' src={bannerIMG2} alt="banner image-2" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
